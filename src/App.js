893f9ce309f5d3e0d1f8238666d1cc7ed38ebdab
lorna gathumbi <nyawiralornah@gmail.com>
2024-11-12 18:51:15 +0300

    maded changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css'
-import Home from './Components/Home';
+import './App.css';
+import Home from './Components/Home'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 function App() {
   return (
-    <Home/>
+  <>
+  <Router>
+    <div>
+      <NavBar/>
+      <Routes>
+        <Route>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={< About/>}/>
+          <Route path='/contact' element={<Contact />}/> 
+          <Route path='/add cats' element={<Home />}/>
+        </Route>
+      </Routes>
+    </div>
+  </Router>
+  </>
   );
 }
 
